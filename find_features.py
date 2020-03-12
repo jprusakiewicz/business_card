93bb2c6ce25a6034677cb8ac57be934f6b47cf48
Kuba Prusakiewicz <jakubprusakiewicz@gmail.com>
2020-03-12 15:47:14 +0100

    feat basic finding features

--- find_features.py
+++ find_features.py
@@ -6,51 +6,75 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from pdf2image import convert_from_path
+import pandas as pd
 
 #DIR = 'tests/test6/'
 #file = 'test6.2.JPG'
 DIR = '../regular_cards/'
-file = 'IMG_5334.JPG'
-file_path = os.path.join(DIR, file)
-doc = fitz.open(file_path+'.pdf')
-page = doc.loadPage(0)
-textpage = page.getTextPage()
-textb = textpage.extractText()
-#regular expression to find emails
-emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", textb, re.IGNORECASE)
+file = 'IMG_5335.JPG'
 
-#regular expression to find phone numbers
-numbers = re.findall(r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]', textb, re.IGNORECASE)
 
-texts_to_draw = []
-texts_to_draw.extend(emails)
-texts_to_draw.extend(numbers)
-print(texts_to_draw)
+def find_features(directory, file_name):
+    file_path = os.path.join(DIR, file_name)
+    doc = fitz.open(file_path + '.pdf')
+    page = doc.loadPage(0)
+    textpage = page.getTextPage()
+    text = textpage.extractText()
+    #regular expression to find emails
+    emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", text, re.IGNORECASE)
 
-boxes = []
-for text in texts_to_draw:
-    boxes += page.searchFor(text)
+    #regular expression to find phone numbers
+    numbers = re.findall(r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]', text, re.IGNORECASE)
 
-print(boxes)
+    names = re.findall(r'[A-Z][a-z]*[\s-][A-Z][a-z]*', text)
+    #print(names)
 
-page_dict = page.getText('dict')
-#page_blocks = page.getText('blocks') unused yet
-page_width = page_dict['width']
-page_height = page_dict['height']
-###
-image = convert_from_path(file_path+'.pdf', size=(page_width, page_height))
+    features_found = []
+    features_found.extend(emails)
+    features_found.extend(numbers)
+    features_found.extend(names)
 
-im = np.array(image[0], dtype=np.uint8)
-fig, ax = plt.subplots(figsize=(30, 20))
+    #print(features_found)
+    #print(len(features_found))
 
-# Display the image
-for rectangle in boxes:
-    rect = patches.Rectangle((rectangle.x0, rectangle.y0), (rectangle.x1 - rectangle.x0), (rectangle.y1 - rectangle.y0),
-                             linewidth=1, edgecolor='red', facecolor='none')
-    ax.add_patch(rect)
+    bounding_boxes = []
+    for text in features_found:
+        bounding_boxes += page.searchFor(text)
 
-ax.imshow(im)
-plt.savefig('foo.png')
+    #print(bounding_boxes)
+    #print(len(bounding_boxes))
+
+    # Create a zipped list of tuples from above lists
+    zippedList = list(zip([file_name], names, numbers, emails))
+    df = pd.DataFrame(zippedList, columns=['Filename', 'Name', 'number', 'email'])
+    #dic = {'name': names, 'number': numbers, 'email': emails}
+    #df = pd.DataFrame(dic)
+    csv = df.to_csv(index=False)
+
+    def draw():
+        page_dict = page.getText('dict')
+        #page_blocks = page.getText('blocks') unused yet
+        page_width = page_dict['width']
+        page_height = page_dict['height']
+
+        image = convert_from_path(file_path+'.pdf', size=(page_width, page_height))
+
+        im = np.array(image[0], dtype=np.uint8)
+        fig, ax = plt.subplots(figsize=(30, 20))
+
+        # Display the image
+        for rectangle in bounding_boxes:
+            rect = patches.Rectangle((rectangle.x0, rectangle.y0), (rectangle.x1 - rectangle.x0), (rectangle.y1 - rectangle.y0),
+                                     linewidth=1, edgecolor='red', facecolor='none')
+            ax.add_patch(rect)
+
+        ax.imshow(im)
+        plt.savefig('foo.JPG')
+    return csv
+    #draw()
+
+a = find_features(directory=DIR, file_name=file)
+print(a)
 #plt.show()
 
 
